bc22c8c63594f80d0135b6c624e76f0ccabbb88d
jon-vdb <jonvdberg8@gmail.com>
2024-05-07 09:38:56 +0700

    return agencies with organisation

--- src/modules/organisation/services.ts
+++ src/modules/organisation/services.ts
@@ -18,29 +18,39 @@ const createOrganisation = async (data: any
     };
 };
 
-const getOrganisationById = async (id: string
-) => {
-    let organisation
-    try{
-        organisation = await prisma.organisation.findUnique({
+const getOrganisationById = async (id: string) => {
+    let agencies
+    try {
+        const organisation = await prisma.organisation.findUnique({
             where: {
                 id
             },
             include: {
                 users: true,
-                agencies: true,
                 students: true,
                 courses: true,
                 requirements: true,
                 agenciesOnOrganisations: true
             }
-        })
+        });
 
-    } catch(e: any) {
-        console.log(e.message)
+        if (organisation && organisation.agenciesOnOrganisations.length) {
+            const agencyIds = organisation.agenciesOnOrganisations.map((agency) => agency.agencyId);
+            agencies = await prisma.agency.findMany({
+                where: {
+                    id: { in: agencyIds }
+                }
+            });
+        }
+
+        return {
+            isValid: !!organisation,
+            data: {...organisation, agencies}
+        };
+    } catch (e: any) {
+        console.error(e.message);
+        return null;
     }
-
-    return organisation;
 };
 
 export default {
